b5c1b6bed762ce5745e06e7479d3c99fa64340cc
igor <idupalmieri@hotmail.com>
2025-05-13 21:57:06 -0300

    first commit

---
+++ portifolio/dados.py
@@ -0,0 +1,29 @@
+habilidades = {
+    'habilidade1':{
+        'nome': 'Python'
+    },
+     'habilidade2':{
+        'nome': 'Django'
+    },
+     'habilidade3':{
+        'nome': 'Java'
+    },
+     'habilidade4':{
+        'nome': 'SpringBoot'
+    },
+     'habilidade5':{
+        'nome': 'Git e Github'
+    },
+
+    
+}
+
+projetos= {
+    'projeto1': {
+        'titulo': 'Cadastro de empresas ', 
+        'descricao': "O Cadastro de empresas é um sistema CRUD desenvolvido para gerenciar listas personalizadas de empresas (ou qualquer outro tipo de relação entre objetos e grupos). O projeto permite organizar empresas em listas específicas, facilitando a categorização e a personalização. Cada empresa tem suas caracteristicas como, nome, email, cnpj",
+        'tecnologia': 'Java, SpingBoot, Spring Data, JPA(Java Persistence API, Hibernate(ORM), Postman)',
+        'github': "https://github.com/IgorARP/CadrastroEmpresas"
+
+    }
+}
